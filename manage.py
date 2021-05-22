2946fb76a331fd8f1badbcfaf62fe6e2d65caaf4
JahiFUGan <hegaoban8484@126.com>
2021-05-22 11:40:39 +0800

    創建項目配置和數據庫配置

--- manage.py
+++ manage.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information3'
+    SQLALCHEMY_TRACK_MODIFICATIONS = Flask
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 
 @app.route('/')
